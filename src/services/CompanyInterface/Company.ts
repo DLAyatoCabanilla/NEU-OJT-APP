8e9e5f2b9423c420ebc664a26ef5acdfcfd1f5ae
RenardMacorol <rmacorol18@gmail.com>
2024-12-07 16:15:18 +0800

    Add Edit Company Functionality

---
+++ src/services/CompanyInterface/Company.ts
@@ -0,0 +1,10 @@
+export interface Company {
+  id: string;
+  CompanyName: string;
+  CompanyAddress: string;
+  RoleInCompany: string;
+  Mode: string;
+  RemainingSlot: number;
+  RegisterdStudents: number;
+
+}
